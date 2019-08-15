b3334a894c3c514a859887110ef868b83fabe8a9
kai zhu <kaizhu256@gmail.com>
2019-08-15 06:28:42 +0000

    require puppeteer/lib/Events.js

--- index.js
+++ index.js
@@ -727,6 +727,90 @@ const closeAsync = Helper.promisify(fs.close);
 
 
 /*
+require puppeteer/lib/Events.js
+*/
+/**
+ * Copyright 2019 Google Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+const Events = {
+  Page: {
+    Close: 'close',
+    Console: 'console',
+    Dialog: 'dialog',
+    DOMContentLoaded: 'domcontentloaded',
+    Error: 'error',
+    // Can't use just 'error' due to node.js special treatment of error events.
+    // @see https://nodejs.org/api/events.html#events_error_events
+    PageError: 'pageerror',
+    Request: 'request',
+    Response: 'response',
+    RequestFailed: 'requestfailed',
+    RequestFinished: 'requestfinished',
+    FrameAttached: 'frameattached',
+    FrameDetached: 'framedetached',
+    FrameNavigated: 'framenavigated',
+    Load: 'load',
+    Metrics: 'metrics',
+    Popup: 'popup',
+    WorkerCreated: 'workercreated',
+    WorkerDestroyed: 'workerdestroyed',
+  },
+
+  Browser: {
+    TargetCreated: 'targetcreated',
+    TargetDestroyed: 'targetdestroyed',
+    TargetChanged: 'targetchanged',
+    Disconnected: 'disconnected'
+  },
+
+  BrowserContext: {
+    TargetCreated: 'targetcreated',
+    TargetDestroyed: 'targetdestroyed',
+    TargetChanged: 'targetchanged',
+  },
+
+  NetworkManager: {
+    Request: Symbol('Events.NetworkManager.Request'),
+    Response: Symbol('Events.NetworkManager.Response'),
+    RequestFailed: Symbol('Events.NetworkManager.RequestFailed'),
+    RequestFinished: Symbol('Events.NetworkManager.RequestFinished'),
+  },
+
+  FrameManager: {
+    FrameAttached: Symbol('Events.FrameManager.FrameAttached'),
+    FrameNavigated: Symbol('Events.FrameManager.FrameNavigated'),
+    FrameDetached: Symbol('Events.FrameManager.FrameDetached'),
+    LifecycleEvent: Symbol('Events.FrameManager.LifecycleEvent'),
+    FrameNavigatedWithinDocument: Symbol('Events.FrameManager.FrameNavigatedWithinDocument'),
+    ExecutionContextCreated: Symbol('Events.FrameManager.ExecutionContextCreated'),
+    ExecutionContextDestroyed: Symbol('Events.FrameManager.ExecutionContextDestroyed'),
+  },
+
+  Connection: {
+    Disconnected: Symbol('Events.Connection.Disconnected'),
+  },
+
+  CDPSession: {
+    Disconnected: Symbol('Events.CDPSession.Disconnected'),
+  },
+};
+
+
+
+/*
 require puppeteer/lib/Browser.js
 */
 /**
@@ -747,7 +831,6 @@ require puppeteer/lib/Browser.js
 
 const {Target} = require('puppeteer/lib/Target');
 const {TaskQueue} = require('puppeteer/lib/TaskQueue');
-const {Events} = require('puppeteer/lib/Events');
 
 class Browser extends EventEmitter {
   /**
@@ -1109,8 +1192,6 @@ class BrowserContext extends EventEmitter {
   }
 }
 
-module.exports = {Browser, BrowserContext};
-
 
 
 /*
@@ -1352,8 +1433,6 @@ function rewriteError(error, message) {
   error.message = message;
   return error;
 }
-
-module.exports = {Connection, CDPSession};
 
 
 
@@ -1675,9 +1754,6 @@ function waitForWSEndpoint(chromeProcess, timeout, preferredRevision) {
  * @property {(?Puppeteer.Viewport)=} defaultViewport
  * @property {number=} slowMo
  */
-
-
-module.exports = Launcher;
 
 
 
