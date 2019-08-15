a1fb10aa9f97ffa02fa0d1621fa0bff8babd6ac1
kai zhu <kaizhu256@gmail.com>
2019-08-15 04:58:21 +0000

    require puppeteer

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const assert = require('assert')
 const child_process = require('child_process')
 const fs = require('fs')
+const moduleCjs = require('module')
 const path = require('path')
 const clone = function (obj) {
     return JSON.parse(JSON.stringify(obj));
@@ -227,7 +228,7 @@ class CovFunction {
 require v8-to-istanbul/lib/script.js
 */
 // Node.js injects a header when executing a script.
-const cjsHeader = require('module').wrapper[0]
+const cjsHeader = moduleCjs.wrapper[0]
 
 class CovScript {
   constructor (scriptPath) {
@@ -390,6 +391,50 @@ class PuppeteerToIstanbul0 {
 const PuppeteerToIstanbul = function (coverageInfo) {
   return new PuppeteerToIstanbul0(coverageInfo)
 }
+
+
+
+/*
+require puppeteer
+*/
+/**
+ * Copyright 2017 Google Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+let asyncawait = true;
+try {
+  new Function('async function test(){await 1}');
+} catch (error) {
+  asyncawait = false;
+}
+
+const {helper} = require('puppeteer/lib/helper');
+const api = require('puppeteer/lib/api');
+for (const className in api) {
+  // Puppeteer-web excludes certain classes from bundle, e.g. BrowserFetcher.
+  if (typeof api[className] === 'function')
+    helper.installAsyncStackHooks(api[className]);
+}
+
+// If node does not support async await, use the compiled version.
+const Puppeteer = require('puppeteer/lib/Puppeteer');
+const packageJson = require('puppeteer/package.json');
+const preferredRevision = packageJson.puppeteer.chromium_revision;
+const isPuppeteerCore = packageJson.name === 'puppeteer-core';
+
+module.exports = new Puppeteer(__dirname, preferredRevision, isPuppeteerCore);
 
 
 
