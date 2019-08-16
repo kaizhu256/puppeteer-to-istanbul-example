8f80442b270fa8e64dd44bb0b1fab2aa0ba1d1d7
kai zhu <kaizhu256@gmail.com>
2019-08-16 00:13:41 -0500

    require puppeteer/lib/ExecutionContext.js

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 "use strict";
 // require builtin
-const EventEmitter = require('events');
-const fs = require('fs')
-const child_process = require('child_process')
-const http = require('http');
-const https = require('https');
-const mime = require('mime');
-const moduleCjs = require('module')
-const os = require('os');
-const path = require('path');
-const readline = require('readline');
-const url = require('url');
+const EventEmitter = require ('events');
+const fs = require ('fs')
+const child_process = require ('child_process')
+const http = require ('http');
+const https = require ('https');
+const mime = require ('mime');
+const moduleCjs = require ('module')
+const os = require ('os');
+const path = require ('path');
+const readline = require ('readline');
+const url = require ('url');
 function assert(value, message) {
 /**
  * @param {*} value
@@ -1935,6 +1935,212 @@ async function waitForPredicatePageFunction(predicateBody, polling, timeout, ...
 
 
 /*
+require puppeteer/lib/ExecutionContext.js
+*/
+/**
+ * Copyright 2017 Google Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+const {createJSHandle, JSHandle} = require('puppeteer/lib/JSHandle');
+
+const EVALUATION_SCRIPT_URL = '__puppeteer_evaluation_script__';
+const SOURCE_URL_REGEX = /^[\040\t]*\/\/[@#] sourceURL=\s*(\S*?)\s*$/m;
+
+class ExecutionContext {
+  /**
+   * @param {!Puppeteer.CDPSession} client
+   * @param {!Protocol.Runtime.ExecutionContextDescription} contextPayload
+   * @param {?Puppeteer.DOMWorld} world
+   */
+  constructor(client, contextPayload, world) {
+    this._client = client;
+    this._world = world;
+    this._contextId = contextPayload.id;
+  }
+
+  /**
+   * @return {?Puppeteer.Frame}
+   */
+  frame() {
+    return this._world ? this._world.frame() : null;
+  }
+
+  /**
+   * @param {Function|string} pageFunction
+   * @param {...*} args
+   * @return {!Promise<*>}
+   */
+  async evaluate(pageFunction, ...args) {
+    return await this._evaluateInternal(true /* returnByValue */, pageFunction, ...args);
+  }
+
+  /**
+   * @param {Function|string} pageFunction
+   * @param {...*} args
+   * @return {!Promise<!JSHandle>}
+   */
+  async evaluateHandle(pageFunction, ...args) {
+    return this._evaluateInternal(false /* returnByValue */, pageFunction, ...args);
+  }
+
+  /**
+   * @param {boolean} returnByValue
+   * @param {Function|string} pageFunction
+   * @param {...*} args
+   * @return {!Promise<*>}
+   */
+  async _evaluateInternal(returnByValue, pageFunction, ...args) {
+    const suffix = `//# sourceURL=${EVALUATION_SCRIPT_URL}`;
+
+    if (isString(pageFunction)) {
+      const contextId = this._contextId;
+      const expression = /** @type {string} */ (pageFunction);
+      const expressionWithSourceUrl = SOURCE_URL_REGEX.test(expression) ? expression : expression + '\n' + suffix;
+      const {exceptionDetails, result: remoteObject} = await this._client.send('Runtime.evaluate', {
+        expression: expressionWithSourceUrl,
+        contextId,
+        returnByValue,
+        awaitPromise: true,
+        userGesture: true
+      }).catch(rewriteError);
+      if (exceptionDetails)
+        throw new Error('Evaluation failed: ' + getExceptionMessage(exceptionDetails));
+      return returnByValue ? valueFromRemoteObject(remoteObject) : createJSHandle(this, remoteObject);
+    }
+
+    if (typeof pageFunction !== 'function')
+      throw new Error(`Expected to get |string| or |function| as the first argument, but got "${pageFunction}" instead.`);
+
+    let functionText = pageFunction.toString();
+    try {
+      new Function('(' + functionText + ')');
+    } catch (e1) {
+      // This means we might have a function shorthand. Try another
+      // time prefixing 'function '.
+      if (functionText.startsWith('async '))
+        functionText = 'async function ' + functionText.substring('async '.length);
+      else
+        functionText = 'function ' + functionText;
+      try {
+        new Function('(' + functionText  + ')');
+      } catch (e2) {
+        // We tried hard to serialize, but there's a weird beast here.
+        throw new Error('Passed function is not well-serializable!');
+      }
+    }
+    let callFunctionOnPromise;
+    try {
+      callFunctionOnPromise = this._client.send('Runtime.callFunctionOn', {
+        functionDeclaration: functionText + '\n' + suffix + '\n',
+        executionContextId: this._contextId,
+        arguments: args.map(convertArgument.bind(this)),
+        returnByValue,
+        awaitPromise: true,
+        userGesture: true
+      });
+    } catch (err) {
+      if (err instanceof TypeError && err.message.startsWith('Converting circular structure to JSON'))
+        err.message += ' Are you passing a nested JSHandle?';
+      throw err;
+    }
+    const { exceptionDetails, result: remoteObject } = await callFunctionOnPromise.catch(rewriteError);
+    if (exceptionDetails)
+      throw new Error('Evaluation failed: ' + getExceptionMessage(exceptionDetails));
+    return returnByValue ? valueFromRemoteObject(remoteObject) : createJSHandle(this, remoteObject);
+
+    /**
+     * @param {*} arg
+     * @return {*}
+     * @this {ExecutionContext}
+     */
+    function convertArgument(arg) {
+      if (typeof arg === 'bigint') // eslint-disable-line valid-typeof
+        return { unserializableValue: `${arg.toString()}n` };
+      if (Object.is(arg, -0))
+        return { unserializableValue: '-0' };
+      if (Object.is(arg, Infinity))
+        return { unserializableValue: 'Infinity' };
+      if (Object.is(arg, -Infinity))
+        return { unserializableValue: '-Infinity' };
+      if (Object.is(arg, NaN))
+        return { unserializableValue: 'NaN' };
+      const objectHandle = arg && (arg instanceof JSHandle) ? arg : null;
+      if (objectHandle) {
+        if (objectHandle._context !== this)
+          throw new Error('JSHandles can be evaluated only in the context they were created!');
+        if (objectHandle._disposed)
+          throw new Error('JSHandle is disposed!');
+        if (objectHandle._remoteObject.unserializableValue)
+          return { unserializableValue: objectHandle._remoteObject.unserializableValue };
+        if (!objectHandle._remoteObject.objectId)
+          return { value: objectHandle._remoteObject.value };
+        return { objectId: objectHandle._remoteObject.objectId };
+      }
+      return { value: arg };
+    }
+
+    /**
+     * @param {!Error} error
+     * @return {!Protocol.Runtime.evaluateReturnValue}
+     */
+    function rewriteError(error) {
+      if (error.message.includes('Object reference chain is too long'))
+        return {result: {type: 'undefined'}};
+      if (error.message.includes('Object couldn\'t be returned by value'))
+        return {result: {type: 'undefined'}};
+
+      if (error.message.endsWith('Cannot find context with specified id'))
+        throw new Error('Execution context was destroyed, most likely because of a navigation.');
+      throw error;
+    }
+  }
+
+  /**
+   * @param {!JSHandle} prototypeHandle
+   * @return {!Promise<!JSHandle>}
+   */
+  async queryObjects(prototypeHandle) {
+    assert(!prototypeHandle._disposed, 'Prototype JSHandle is disposed!');
+    assert(prototypeHandle._remoteObject.objectId, 'Prototype JSHandle must not be referencing primitive value');
+    const response = await this._client.send('Runtime.queryObjects', {
+      prototypeObjectId: prototypeHandle._remoteObject.objectId
+    });
+    return createJSHandle(this, response.objects);
+  }
+
+  /**
+   * @param {Puppeteer.ElementHandle} elementHandle
+   * @return {Promise<Puppeteer.ElementHandle>}
+   */
+  async _adoptElementHandle(elementHandle) {
+    assert(elementHandle.executionContext() !== this, 'Cannot adopt handle that already belongs to this execution context');
+    assert(this._world, 'Cannot adopt handle without DOMWorld');
+    const nodeInfo = await this._client.send('DOM.describeNode', {
+      objectId: elementHandle._remoteObject.objectId,
+    });
+    const {object} = await this._client.send('DOM.resolveNode', {
+      backendNodeId: nodeInfo.node.backendNodeId,
+      executionContextId: this._contextId,
+    });
+    return /** @type {Puppeteer.ElementHandle}*/(createJSHandle(this, object));
+  }
+}
+
+
+
+/*
 require puppeteer/lib/FrameManager.js
 */
 /**
@@ -1954,7 +2160,6 @@ require puppeteer/lib/FrameManager.js
  */
 
 const {Events} = require('puppeteer/lib/Events');
-const {ExecutionContext, EVALUATION_SCRIPT_URL} = require('puppeteer/lib/ExecutionContext');
 const {LifecycleWatcher} = require('puppeteer/lib/LifecycleWatcher');
 const {NetworkManager} = require('puppeteer/lib/NetworkManager');
 
@@ -2994,7 +3199,6 @@ require puppeteer/lib/Page.js
 const {EmulationManager} = require('puppeteer/lib/EmulationManager');
 const {Coverage} = require('puppeteer/lib/Coverage');
 const {Worker} = require('puppeteer/lib/Worker');
-const {createJSHandle} = require('puppeteer/lib/JSHandle');
 const {TimeoutSettings} = require('puppeteer/lib/TimeoutSettings');
 const writeFileAsync = promisify(fs.writeFile);
 
