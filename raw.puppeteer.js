232610df9dbf50e24be70c2476dc7fbead2973d8
kai zhu <kaizhu256@gmail.com>
2019-08-16 01:13:41 -0500

    raw.puppeteer.js

---
+++ raw.puppeteer.js
@@ -0,0 +1,5 @@
+/*
+file https://registry.npmjs.org/puppeteer/-/puppeteer-1.19.0.tgz
+shGithubDateCommitted https://github.com/GoogleChrome/puppeteer/commits/v1.19.0 # 2019-07-23T05:02:45Z
+curl https://registry.npmjs.org/puppeteer/-/puppeteer-1.19.0.tgz | tar -O -xz package/dist/puppeteer.js > /tmp/aa.js
+*/
