c5d2fba21ec2137ead27c3e22aa1f52340d0030b
kai zhu <kaizhu256@gmail.com>
2019-08-24 22:02:42 +0000

    cull.globalthis

--- lib.puppeteer.screenshot.js
+++ lib.puppeteer.screenshot.js
@@ -11,148 +11,6 @@
 ///* jslint utility2:true */
 
 /* jslint ignore:start */
-(function (globalThis) {
-        "use strict";
-        var consoleError;
-        var local;
-        // init globalThis
-        (function () {
-                try {
-                        globalThis = Function("return this")(); // jslint ignore:line
-                } catch (ignore) {}
-        }());
-        globalThis.globalThis = globalThis;
-        // init debug_inline
-        if (!globalThis["debug\u0049nline"]) {
-                consoleError = console.error;
-                globalThis["debug\u0049nline"] = function () {
-                /*
-                  * this function will both print <arguments> to stderr
-                  * and return <arguments>[0]
-                  */
-                        var argList;
-                        argList = Array.from(arguments); // jslint ignore:line
-                        // debug arguments
-                        globalThis["debug\u0049nlineArguments"] = argList;
-                        consoleError("\n\ndebug\u0049nline");
-                        consoleError.apply(console, argList);
-                        consoleError("\n");
-                        // return arg0 for inspection
-                        return argList[0];
-                };
-        }
-        // init local
-        local = {};
-        local.local = local;
-        globalThis.globalLocal = local;
-        // init isBrowser
-        local.isBrowser = (
-                typeof window === "object"
-                && window === globalThis
-                && typeof window.XMLHttpRequest === "function"
-                && window.document
-                && typeof window.document.querySelector === "function"
-        );
-        // init function
-        local.assertThrow = function (passed, message) {
-        /*
-          * this function will throw err.<message> if <passed> is falsy
-          */
-                var err;
-                if (passed) {
-                        return;
-                }
-                err = (
-                        // ternary-operator
-                        (
-                                message
-                                && typeof message.message === "string"
-                                && typeof message.stack === "string"
-                        )
-                        // if message is errObj, then leave as is
-                        ? message
-                        : new Error(
-                                typeof message === "string"
-                                // if message is a string, then leave as is
-                                ? message
-                                // else JSON.stringify message
-                                : JSON.stringify(message, null, 4)
-                        )
-                );
-                throw err;
-        };
-        local.functionOrNop = function (fnc) {
-        /*
-          * this function will if <fnc> exists,
-          * them return <fnc>,
-          * else return <nop>
-          */
-                return fnc || local.nop;
-        };
-        local.identity = function (value) {
-        /*
-          * this function will return <value>
-          */
-                return value;
-        };
-        local.nop = function () {
-        /*
-          * this function will do nothing
-          */
-                return;
-        };
-        local.objectAssignDefault = function (target, source) {
-        /*
-          * this function will if items from <target> are
-          * null, undefined, or empty-string,
-          * then overwrite them with items from <source>
-          */
-                target = target || {};
-                Object.keys(source || {}).forEach(function (key) {
-                        if (
-                                target[key] === null
-                                || target[key] === undefined
-                                || target[key] === ""
-                        ) {
-                                target[key] = target[key] || source[key];
-                        }
-                });
-                return target;
-        };
-        // require builtin
-        if (!local.isBrowser) {
-                local.assert = require("assert");
-                local.buffer = require("buffer");
-                local.child_process = require("child_process");
-                local.cluster = require("cluster");
-                local.crypto = require("crypto");
-                local.dgram = require("dgram");
-                local.dns = require("dns");
-                local.domain = require("domain");
-                local.events = require("events");
-                local.fs = require("fs");
-                local.http = require("http");
-                local.https = require("https");
-                local.net = require("net");
-                local.os = require("os");
-                local.path = require("path");
-                local.querystring = require("querystring");
-                local.readline = require("readline");
-                local.repl = require("repl");
-                local.stream = require("stream");
-                local.string_decoder = require("string_decoder");
-                local.timers = require("timers");
-                local.tls = require("tls");
-                local.tty = require("tty");
-                local.url = require("url");
-                local.util = require("util");
-                local.vm = require("vm");
-                local.zlib = require("zlib");
-        }
-}(this));
-
-
-
 (function (local) {
 "use strict";
 // hack-puppeteer - module.exports
@@ -180,31 +38,6 @@ const debugError = console.error;
 const debugProtocol = function () {
         return;
 }
-const removeFolder = function (dir, onError) {
-/*
-  * this function will asynchronously "rm -fr" <dir>
-  */
-        child_process.spawn("rm", [
-                "-fr", path.resolve(process.cwd(), dir)
-        ], {
-                stdio: [
-                        "ignore", 1, 2
-                ]
-        }).on("exit", onError);
-};
-removeFolder.sync = function (dir) {
-/*
-  * this function will synchronously "rm -fr" <dir>
-  */
-        child_process.spawnSync("rm", [
-                "-fr", path.resolve(process.cwd(), dir)
-        ], {
-                stdio: [
-                        "ignore", 1, 2
-                ]
-        });
-};
-const removeRecursive = removeFolder;
 const timeout = 30000;
 
 
@@ -228,8 +61,6 @@ file https://github.com/websockets/ws/tree/6.2.1
 // require('./permessage-deflate') // const PerMessageDeflate = require('./permessage-deflate');
 // require('./receiver') // const Receiver = require('./receiver');
 // require('./sender') // const Sender = require('./sender');
-// require('./validation') // const { isValidStatusCode } = require('./validation');
-// require('./validation') // const { isValidStatusCode, isValidUTF8 } = require('./validation');
 // require('./websocket') // const WebSocket = require('./websocket');
 // require('async-limiter') // const Limiter = require('async-limiter');
 // require('bufferutil') // const bufferUtil = require('bufferutil');
@@ -254,25 +85,6 @@ lib https://github.com/websockets/ws/blob/6.2.1/validation.js
 
 // hack-puppeteer - module.exports
 const isValidUTF8 = () => true;
-
-/**
-  * Checks if a status code is allowed in a close frame.
-  *
-  * @param {Number} code The status code
-  * @return {Boolean} `true` if the status code is valid, else `false`
-  * @public
-  */
-// hack-puppeteer - module.exports
-const isValidStatusCode = (code) => {
-    return (
-        (code >= 1000 &&
-            code <= 1013 &&
-            code !== 1004 &&
-            code !== 1005 &&
-            code !== 1006) ||
-        (code >= 3000 && code <= 4999)
-    );
-};
 
 
 
@@ -578,7 +390,6 @@ lib https://github.com/websockets/ws/blob/6.2.1/receiver.js
     // kWebSocket
 // } = require('./constants');
 // const { concat, toArrayBuffer, unmask } = require('./buffer-util');
-// const { isValidStatusCode, isValidUTF8 } = require('./validation');
 
 const GET_INFO = 0;
 const GET_PAYLOAD_LENGTH_16 = 1;
@@ -2012,7 +1823,6 @@ file https://github.com/GoogleChrome/puppeteer/tree/v1.19.0
 // require('path') // const path = require('path');
 // require('proxy-from-env') // const getProxyForUrl = require('proxy-from-env').getProxyForUrl;
 // require('readline') // const readline = require('readline');
-// require('rimraf') // const removeFolder = require('rimraf');
 // require('rimraf') // const removeRecursive = require('rimraf');
 // require('url') // const URL = require('url');
 // require('util') // const util = require('util');
