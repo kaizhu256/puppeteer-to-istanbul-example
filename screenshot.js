13eaa192f03ec86906f022a78e8c7a1b92a9a385
kai zhu <kaizhu256@gmail.com>
2019-08-24 16:14:11 +0000

    screenshot.js

--- screenshot.js
+++ screenshot.js
@@ -1,13 +1,154 @@
 /* jslint utility2:true */
+(function (globalThis) {
+    "use strict";
+    var consoleError;
+    var local;
+    // init globalThis
+    globalThis.globalThis = globalThis.globalThis || globalThis;
+    // init debug_inline
+    if (!globalThis["debug\u0049nline"]) {
+        consoleError = console.error;
+        globalThis["debug\u0049nline"] = function () {
+        /*
+         * this function will both print <arguments> to stderr
+         * and return <arguments>[0]
+         */
+            var argList;
+            argList = Array.from(arguments); // jslint ignore:line
+            // debug arguments
+            globalThis["debug\u0049nlineArguments"] = argList;
+            consoleError("\n\ndebug\u0049nline");
+            consoleError.apply(console, argList);
+            consoleError("\n");
+            // return arg0 for inspection
+            return argList[0];
+        };
+    }
+    // init local
+    local = {};
+    local.local = local;
+    globalThis.globalLocal = local;
+    // init isBrowser
+    local.isBrowser = (
+        typeof window === "object"
+        && window === globalThis
+        && typeof window.XMLHttpRequest === "function"
+        && window.document
+        && typeof window.document.querySelector === "function"
+    );
+    // init function
+    local.assertThrow = function (passed, message) {
+    /*
+     * this function will throw err.<message> if <passed> is falsy
+     */
+        var err;
+        if (passed) {
+            return;
+        }
+        err = (
+            (
+                message
+                && typeof message.message === "string"
+                && typeof message.stack === "string"
+            )
+            // if message is errObj, then leave as is
+            ? message
+            : new Error(
+                typeof message === "string"
+                // if message is a string, then leave as is
+                ? message
+                // else JSON.stringify message
+                : JSON.stringify(message, null, 4)
+            )
+        );
+        throw err;
+    };
+    local.functionOrNop = function (fnc) {
+    /*
+     * this function will if <fnc> exists,
+     * them return <fnc>,
+     * else return <nop>
+     */
+        return fnc || local.nop;
+    };
+    local.identity = function (value) {
+    /*
+     * this function will return <value>
+     */
+        return value;
+    };
+    local.nop = function () {
+    /*
+     * this function will do nothing
+     */
+        return;
+    };
+    local.objectAssignDefault = function (target, source) {
+    /*
+     * this function will if items from <target> are
+     * null, undefined, or empty-string,
+     * then overwrite them with items from <source>
+     */
+        target = target || {};
+        Object.keys(source || {}).forEach(function (key) {
+            if (
+                target[key] === null
+                || target[key] === undefined
+                || target[key] === ""
+            ) {
+                target[key] = target[key] || source[key];
+            }
+        });
+        return target;
+    };
+    // require builtin
+    if (!local.isBrowser) {
+        local.assert = require("assert");
+        local.buffer = require("buffer");
+        local.child_process = require("child_process");
+        local.cluster = require("cluster");
+        local.crypto = require("crypto");
+        local.dgram = require("dgram");
+        local.dns = require("dns");
+        local.domain = require("domain");
+        local.events = require("events");
+        local.fs = require("fs");
+        local.http = require("http");
+        local.https = require("https");
+        local.net = require("net");
+        local.os = require("os");
+        local.path = require("path");
+        local.querystring = require("querystring");
+        local.readline = require("readline");
+        local.repl = require("repl");
+        local.stream = require("stream");
+        local.string_decoder = require("string_decoder");
+        local.timers = require("timers");
+        local.tls = require("tls");
+        local.tty = require("tty");
+        local.url = require("url");
+        local.util = require("util");
+        local.vm = require("vm");
+        local.zlib = require("zlib");
+    }
+}(
+    typeof globalThis === "object"
+    ? globalThis
+    : (function () {
+        return Function("return this")(); // jslint ignore:line
+    }())
+));
+
+
 
 (async function (local) {
 "use strict";
 //!! // hack-puppeteer - module.exports
 //!! const EventEmitter = require("events");
 //!! const URL = require("url");
-const child_process = require("child_process");
+local.child_process = require("child_process");
 //!! const crypto = require("crypto");
-const fs = require("fs");
+local.fs = require("fs");
 //!! const http = require("http");
 //!! const https = require("https");
 //!! const net = require("net");
@@ -21,8 +162,6 @@ const fs = require("fs");
 //!! const { randomBytes} = require("crypto");
 
 module.exports = require("./.a00.js");
-
-local = {};
 
 var browser;
 var chromeClosed;
@@ -56,7 +195,7 @@ local.killChrome = function () {
         // Force kill chrome.
         try {
             if (process.platform === "win32") {
-                child_process.execSync(
+                local.child_process.execSync(
                     `taskkill /pid ${chromeProcess.pid} /T /F`
                 );
             } else {
@@ -72,7 +211,7 @@ local.killChrome130 = function () {
     process.exit(130);
 };
 
-chromeProcess = child_process.spawn((
+chromeProcess = local.child_process.spawn((
     "node_modules/puppeteer/.local-chromium"
     + "/linux-674921/chrome-linux/chrome"
 ), [
@@ -83,7 +222,6 @@ chromeProcess = child_process.spawn((
     "--mute-audio",
     "--no-sandbox",
     "--remote-debugging-port=0"
-    //!! `--user-data-dir=${temporaryUserDataDir}`
 ], {
     // On non-windows platforms, `detached: false` makes child process
     // a leader of a new process group, making it possible
@@ -146,6 +284,6 @@ await page.goto("https://www.example.com");
 await page.screenshot({
     path: "tmp/aa.png"
 });
-fs.writeFileSync("tmp/aa.html", await page.content());
+local.fs.writeFileSync("tmp/aa.html", await page.content());
 await browser.close();
-}());
+}(globalThis.globalLocal));
