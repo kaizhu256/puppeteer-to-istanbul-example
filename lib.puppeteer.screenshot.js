fab8255be37f1eaa1706e5ff8c5bbf7db3e0a5e5
kai zhu <kaizhu256@gmail.com>
2019-08-25 08:19:28 +0000

    cull.websockettransport

--- lib.puppeteer.screenshot.js
+++ lib.puppeteer.screenshot.js
@@ -2212,46 +2212,23 @@ module.exports = {Target};
 
 
 
-/*
-lib https://github.com/GoogleChrome/puppeteer/blob/v1.19.0/WebSocketTransport.js
-*/
-/**
-  * Copyright 2018 Google Inc. All rights reserved.
-  *
-  * Licensed under the Apache License, Version 2.0 (the "License");
-  * you may not use this file except in compliance with the License.
-  * You may obtain a copy of the License at
-  *
-  *     http://www.apache.org/licenses/LICENSE-2.0
-  *
-  * Unless required by applicable law or agreed to in writing, software
-  * distributed under the License is distributed on an "AS IS" BASIS,
-  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  * See the License for the specific language governing permissions and
-  * limitations under the License.
-  */
-// const WebSocket = require('ws');
-
-/**
-  * @implements {!Puppeteer.ConnectionTransport}
-  */
-function WebSocketTransport (ws) {
-    ws.addEventListener("message", function (event) {
-        ws.onmessage.call(null, event.data);
-    });
-    ws.addEventListener("close", function (event) {
-        ws.onclose.call(null);
-    });
-    // Silently ignore all errors - we don't know what to do with them.
-    ws.addEventListener("error", () => {});
-    return ws
+function WebSocketTransport () {
+    return;
 }
 WebSocketTransport.create = function (url) {
     return new Promise(function (resolve, reject) {
         const ws = new WebSocket(url, [], {
             maxPayload: 256 * 1024 * 1024, // 256Mb
         });
-        ws.addEventListener("open", () => resolve(WebSocketTransport(ws)));
+        ws.addEventListener("message", function (event) {
+            ws.onmessage.call(null, event.data);
+        });
+        ws.addEventListener("close", function (event) {
+            ws.onclose.call(null);
+        });
+        ws.addEventListener("open", function () {
+            resolve(ws);
+        });
         ws.addEventListener("error", reject);
     });
 }
