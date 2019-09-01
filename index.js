3e0bfa2d337015c7ebf0ab8905ffa18f89f1430c
kai zhu <kaizhu256@gmail.com>
2019-09-01 04:07:53 +0000

    standalone.vanilla

--- index.js
+++ index.js
@@ -1,23 +1,245 @@
+(function (globalThis) {
+    "use strict";
+    var consoleError;
+    var local;
+    // init globalThis
+    globalThis.globalThis = globalThis.globalThis || globalThis;
+    // init debug_inline
+    if (!globalThis["debug\u0049nline"]) {
+        consoleError = console.error;
+        globalThis["debug\u0049nline"] = function () {
+        /*
+         * this function will both print <arguments> to stderr
+         * and return <arguments>[0]
+         */
+            var argList;
+            argList = Array.from(arguments); // jslint ignore:line
+            // debug arguments
+            globalThis["debug\u0049nlineArguments"] = argList;
+            consoleError("\n\ndebug\u0049nline");
+            consoleError.apply(console, argList);
+            consoleError("\n");
+            // return arg0 for inspection
+            return argList[0];
+        };
+    }
+    // init local
+    local = {};
+    local.local = local;
+    globalThis.globalLocal = local;
+    // init isBrowser
+    local.isBrowser = (
+        typeof window === "object"
+        && window === globalThis
+        && typeof window.XMLHttpRequest === "function"
+        && window.document
+        && typeof window.document.querySelector === "function"
+    );
+    // init function
+    local.assertThrow = function (passed, message) {
+    /*
+     * this function will throw err.<message> if <passed> is falsy
+     */
+        var err;
+        if (passed) {
+            return;
+        }
+        err = (
+            (
+                message
+                && typeof message.message === "string"
+                && typeof message.stack === "string"
+            )
+            // if message is errObj, then leave as is
+            ? message
+            : new Error(
+                typeof message === "string"
+                // if message is a string, then leave as is
+                ? message
+                // else JSON.stringify message
+                : JSON.stringify(message, null, 4)
+            )
+        );
+        throw err;
+    };
+    local.fsRmrfSync = function (dir) {
+    /*
+     * this function will sync "rm -rf" <dir>
+     */
+        var child_process;
+        try {
+            child_process = require("child_process");
+        } catch (ignore) {
+            return;
+        }
+        child_process.spawnSync("rm", [
+            "-rf", dir
+        ], {
+            stdio: [
+                "ignore", 1, 2
+            ]
+        });
+    };
+    local.fsWriteFileWithMkdirpSync = function (file, data) {
+    /*
+     * this function will sync write <data> to <file> with "mkdir -p"
+     */
+        var fs;
+        try {
+            fs = require("fs");
+        } catch (ignore) {
+            return;
+        }
+        // try to write file
+        try {
+            fs.writeFileSync(file, data);
+        } catch (ignore) {
+            // mkdir -p
+            require("child_process").spawnSync(
+                "mkdir",
+                [
+                    "-p", require("path").dirname(file)
+                ],
+                {
+                    stdio: [
+                        "ignore", 1, 2
+                    ]
+                }
+            );
+            // rewrite file
+            fs.writeFileSync(file, data);
+        }
+    };
+    local.functionOrNop = function (fnc) {
+    /*
+     * this function will if <fnc> exists,
+     * them return <fnc>,
+     * else return <nop>
+     */
+        return fnc || local.nop;
+    };
+    local.identity = function (value) {
+    /*
+     * this function will return <value>
+     */
+        return value;
+    };
+    local.nop = function () {
+    /*
+     * this function will do nothing
+     */
+        return;
+    };
+    local.objectAssignDefault = function (target, source) {
+    /*
+     * this function will if items from <target> are
+     * null, undefined, or empty-string,
+     * then overwrite them with items from <source>
+     */
+        target = target || {};
+        Object.keys(source || {}).forEach(function (key) {
+            if (
+                target[key] === null
+                || target[key] === undefined
+                || target[key] === ""
+            ) {
+                target[key] = target[key] || source[key];
+            }
+        });
+        return target;
+    };
+    // require builtin
+    if (!local.isBrowser) {
+        local.assert = require("assert");
+        local.buffer = require("buffer");
+        local.child_process = require("child_process");
+        local.cluster = require("cluster");
+        local.crypto = require("crypto");
+        local.dgram = require("dgram");
+        local.dns = require("dns");
+        local.domain = require("domain");
+        local.events = require("events");
+        local.fs = require("fs");
+        local.http = require("http");
+        local.https = require("https");
+        local.net = require("net");
+        local.os = require("os");
+        local.path = require("path");
+        local.querystring = require("querystring");
+        local.readline = require("readline");
+        local.repl = require("repl");
+        local.stream = require("stream");
+        local.string_decoder = require("string_decoder");
+        local.timers = require("timers");
+        local.tls = require("tls");
+        local.tty = require("tty");
+        local.url = require("url");
+        local.util = require("util");
+        local.vm = require("vm");
+        local.zlib = require("zlib");
+    }
+}((typeof globalThis === "object" && globalThis) || (function () {
+    return Function("return this")(); // jslint ignore:line
+}())));
+
+
+
+(async function (local) {
+"use strict"
+// artifact - reset
+local.fsRmrfSync("coverage");
+local.fsRmrfSync(".nyc_output");
+
+const fs = require('fs')
 const path = require('path')
 const pti = require('puppeteer-to-istanbul')
+const puppeteer = require('puppeteer')
+const browser = await puppeteer.launch({
+    args: [
+        // "--no-sandbox", "--disable-setuid-sandbox"
+        "--disable-setuid-sandbox",
+        "--headless",
+        "--hide-scrollbars",
+        "--incognito",
+        "--mute-audio",
+        "--no-sandbox",
+        "--remote-debugging-port=0"
+    ],
+    dumpio: true,
+    executablePath: (
+        "node_modules/puppeteer/.local-chromium"
+        + "/linux-674921/chrome-linux/chrome"
+    ),
+    headless: true
+})
+const page = await browser.newPage()
 
-;(async () => {
-  const puppeteer = require('puppeteer')
-  const browser = await puppeteer.launch()
-  const page = await browser.newPage()
-
-  // Enable both JavaScript and CSS coverage
-  await Promise.all([
+// Enable both JavaScript and CSS coverage
+await Promise.all([
     page.coverage.startJSCoverage(),
     page.coverage.startCSSCoverage()
-  ])
+])
 
-  // Navigate to page
-  let url = 'file:///' + path.resolve('./index.html')
-  await page.goto(url)
+// Navigate to page
+let url = 'file:///' + path.resolve('./index.html')
+url = "https://m.youtube.com";
+await page.goto(url)
+// browser - wait 2000 ms
+await new Promise(function (resolve) {
+    setTimeout(resolve, 5000);
+});
 
-  // Disable JavaScript coverage
-  const jsCoverage = await page.coverage.stopJSCoverage()
-  pti.write(jsCoverage)
-  await browser.close()
-})()
+// Disable JavaScript coverage
+const jsCoverage = await page.coverage.stopJSCoverage()
+pti.write(jsCoverage)
+await browser.close()
+
+// nyc - coverage-report
+process.argv = [
+    "/usr/bin/node",
+    "/root/Documents/puppeteer-to-istanbul-example/node_modules/.bin/nyc",
+    "report",
+    "--reporter=html"
+];
+require("./node_modules/nyc/bin/nyc.js");
+}(globalThis.globalLocal));
